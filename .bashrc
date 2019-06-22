8c9ec439f70bc0e6d7825f2fe3a076d350c8dfa3
hiroki hakeda <hirokihakeda@hirokinoMacBook-Pro.local>
2019-06-22 18:25:19 +0900

    fix

---
+++ .bashrc
@@ -0,0 +1,2 @@
+
+exportPATH=/usr/local/node−v0.12.7−linux−x86/bin/:exportPATH=/usr/local/node−v0.12.7−linux−x86/bin/:PATH
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get 'pages' => 'pages#index'
+root 'pages#index'
 get 'pages/co_info' => 'pages#co_info'
 get 'pages/contact' => 'pages#contact'
 get 'pages/service' => 'pages#service'
 end
+
